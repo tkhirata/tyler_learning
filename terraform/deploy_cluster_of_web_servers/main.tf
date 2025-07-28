275b9febc2b383f4f3312252ab1ffcfcf68e5f54
tyler.hirata <tyler.hirata@riotgames.com>
2025-07-27 22:18:48 -0700

    Adding Launch Configuration

---
+++ terraform/deploy_cluster_of_web_servers/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "ConfigurableWebserver" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "hi"
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p ${var.server_port} &
+            EOF
+
+  user_data_replace_on_change = true
+
+
+  tags = {
+    Name = "ConfigurableWebserver"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-ConfigurableWebserver-example-instance"
+
+  ingress{
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Code to create Launch Configuration
+
+resource "aws_launch_configuration" "example" {
+  image_id = var.ami
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+            #!/bin/bash
+            echo "hi"
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p ${var.server_port} &
+            EOF
+}
