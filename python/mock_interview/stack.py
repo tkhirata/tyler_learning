031c7baa8fab68c440b09ab8b58d5c1766b3dfb4
tyler.hirata <tyler.hirata@riotgames.com>
2025-08-13 10:10:06 -0700

    adding version with solution

--- python/mock_interview/stack.py
+++ python/mock_interview/stack.py
@@ -6,15 +6,22 @@ def parenthesis (input_string):
             open_parenthesis.append(char)
 
         if char == ')' or char == '}' or char == ']':
+            if len(open_parenthesis) > 0 and open_parenthesis[-1] == '(' and char == ')':
+                open_parenthesis.pop()
+            elif len(open_parenthesis) == 0:
+                open_parenthesis.append(char)
 
-            if open_parenthesis[-1] == '(' and char == ')':
+            elif len(open_parenthesis) > 0 and open_parenthesis[-1] == '{' and char == '}':
                 open_parenthesis.pop()
 
-            if len(open_parenthesis) > 0 and open_parenthesis[-1] == '{' and char == '}':
-                open_parenthesis.pop()
+            elif len(open_parenthesis) == 0:
+                open_parenthesis.append(char)
 
-            if len(open_parenthesis) > 0 and open_parenthesis[-1] == '[' and char == ']':
+            elif len(open_parenthesis) > 0 and open_parenthesis[-1] == '[' and char == ']':
                 open_parenthesis.pop()
+            else:
+                open_parenthesis.append(char)
+
 
     if len(open_parenthesis) == 0:
         print('valid!!!')
@@ -22,10 +29,11 @@ def parenthesis (input_string):
     if len(open_parenthesis) > 0:
         print('invalid!!!')
 
-#parenthesis('([)]')
-#parenthesis('[(])')
-#parenthesis('a[Bx]}d(e){Q}}')
-#parenthesis('({[()]}[({)})')
-#parenthesis('({[()][]})')
-#parenthesis('{[({[]})]}({[]})[({})]{()}')
-parenthesis('{[({[]})]}({[})][({})]{()}')
+
+#parenthesis("()[]")
+parenthesis("()")
+
+
+# run through two examples, one that works and one that does
+
+# code should return False if it cant do the comparison
